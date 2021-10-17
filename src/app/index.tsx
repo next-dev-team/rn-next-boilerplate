e863d1f553c38be251765a3837eb598e1c1ddaae
sial <rimsila.itc@gmail.com>
2021-10-17 18:46:59 +0700

    remove redux

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from '@shopify/restyle';
 import { Fallback } from 'components/atoms/Fallback';
-import Spinning from 'components/atoms/Spinning';
 import 'i18n/index';
 import { configure } from 'mobx';
 import { linking } from 'navigation/linking';
 import { navigationRef } from 'navigation/NavigationService';
 import React, { Suspense } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from 'store/store';
 import { darkTheme, lightTheme } from 'themes';
 import AppNavigation from '../navigation/AppNavigation';
 
@@ -25,15 +21,11 @@ const App = () => {
   return (
     <Suspense fallback={null}>
       <SafeAreaProvider>
-        <Provider store={store}>
-          <ThemeProvider theme={false ? darkTheme : lightTheme}>
-            <PersistGate loading={<Spinning visible />} persistor={persistor}>
-              <NavigationContainer linking={linking} ref={navigationRef} fallback={<Fallback />}>
-                <AppNavigation />
-              </NavigationContainer>
-            </PersistGate>
-          </ThemeProvider>
-        </Provider>
+        <ThemeProvider theme={false ? darkTheme : lightTheme}>
+          <NavigationContainer linking={linking} ref={navigationRef} fallback={<Fallback />}>
+            <AppNavigation />
+          </NavigationContainer>
+        </ThemeProvider>
       </SafeAreaProvider>
     </Suspense>
   );
--- src/store/rootMiddleware.ts
+++
@@ -1,5 +0,0 @@
-/**
- * if have someMiddleware
- */
-// [someMiddleware]
-export default [];
