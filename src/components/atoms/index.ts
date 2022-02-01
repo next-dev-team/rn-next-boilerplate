f174370bd64b385c7c3fadf7a51d238773c8f9ea
Sila Rim <51617383+rimsila@users.noreply.github.com>
2022-02-01 23:29:31 +0700

    refactor: file name

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,7 +1,8 @@
 export { Box, IBox } from './box';
 export { default as Button } from './button';
 export { default as Image } from './image/Image';
+export { KeyboardAwareScrollView } from './keyboardAwareScrollView1';
 export { ContainerRoot, ISection, Layout, Section } from './layout';
 export { default as SvgIcon, IconNames, SvgIconProps } from './svg-icon';
-export { default as Text } from './text';
-export { default as TextBg } from './text/textBg';
+export { default as Text } from './text1';
+export { default as TextBg } from './text1/textBg';
